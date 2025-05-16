aa524a78bbd3035b2726cfee7702d3555edc9146
Антон <156504410+Paradicc@users.noreply.github.com>
2025-05-16 09:28:49 +0300

    script.js

--- script.js
+++ script.js
@@ -149,6 +149,21 @@ const App = () => {
     </div>
   );
 };
+// Плавный скролл по якорям
+document.querySelectorAll('.nav a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+      e.preventDefault();
 
+      const targetId = this.getAttribute('href');
+      const targetElement = document.querySelector(targetId);
+
+      if (targetElement) {
+          window.scrollTo({
+              top: targetElement.offsetTop,
+              behavior: 'smooth'
+          });
+      }
+  });
+});
 ReactDOM.render(<App />, document.getElementById('root'));
 
